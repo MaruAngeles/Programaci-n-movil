58020a601dbe521e51cefb642755125b10dd2cb7
MaruAngeles <marurufi999@gmail.com>
2026-06-08 18:57:50 -0600

    Practica 6

--- Practica5/myApp2/App.js
+++ Practica5/myApp2/App.js
@@ -1,15 +1,28 @@
+/*Zona 1: Importancia de archivos y Componentes */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Saludo} from './components/Saludo';
+import { Saludos2 } from './components/Saludos2';
 
+/*Zona 2: Main -Componentes */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+
+      <Image source={require('./assets/wave.png')}/>
+      <Text>Hola Mundo React Native </Text>
+      <Text> ------------------------------------- </Text>
+      <Saludo/>
+
+      <Text> ------------------------------------- </Text>
+      <Saludos2/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/*Zona 3:Estilos y posicionamiento */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
